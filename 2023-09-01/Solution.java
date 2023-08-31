02f29afe32150a0db2d5d40ffd10782e325f3593
Anagha Shenoy <44674504+AnaghaShenoy@users.noreply.github.com>
2023-09-01 02:30:19 +0530

    Update Solution.java

--- 2023-09-01/Solution.java
+++ 2023-09-01/Solution.java
@@ -1,4 +1,4 @@
-``class Solution {
+class Solution {
     public String mergeAlternately(String word1, String word2) {
         StringBuilder ans = new StringBuilder();
         int n = word1.length();
@@ -13,4 +13,4 @@
         ans.append(word1.substring(i, n)).append(word2.substring(i,m));
         return ans.toString();
     }
-}``
+}
